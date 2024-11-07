062eb3fca860b2ca6ee0daf84affd17a93aab2ea
etabebe1 <etabebe1@gmail.com>
2024-11-07 03:34:03 -0500

    Updated subproject commit in lib/forge-std and reformatted whitespace in DataStorage.sol

--- src/DataStorage.sol
+++ src/DataStorage.sol
@@ -6,23 +6,23 @@ contract DataStorage {
 
     struct UserData {
         uint256 usernumber;
-        string username; 
+        string username;
     }
 
     UserData[] public userDataArray;
 
     mapping (string => uint256) public usernameToUsernumber;
 
-    function addUser(uint256 _usernumber, string memory _username) public  { 
+    function addUser(uint256 _usernumber, string memory _username) public {
         userDataArray.push(UserData(_usernumber,  _username));
         usernameToUsernumber[_username] = _usernumber;
     }
 
-    function storeNumber (uint256 _number) public virtual  {
+    function storeNumber(uint256 _number) public virtual {
         myNumber = _number;
     }
 
-    function getNumber () public view returns(uint256) {
+    function getNumber() public view returns(uint256) {
         return myNumber;
     }
 }
